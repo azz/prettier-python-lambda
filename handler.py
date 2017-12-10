e2dea38b0dd2ab9df43b957ee1611b4f0785af95
Lucas Azzola <derflatulator@gmail.com>
2017-12-11 00:21:07 +1100

    fix: add cors header

--- handler.py
+++ handler.py
@@ -9,6 +9,9 @@ def parse(event, context = None):
 
     response = {
         "statusCode": 200,
+        "headers": {
+            "Access-Control-Allow-Origin": "*"
+        },
         "body": serialized
     }
 
